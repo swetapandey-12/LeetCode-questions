6186b212cd48a865b7ab478c0be1bd66bc1c6a7e
sweta pandey <74544867+swetapandey-12@users.noreply.github.com>
2022-07-28 15:38:56 +0530

    Create 242. Valid Anagram.cpp

---
+++ 242. Valid Anagram.cpp
@@ -0,0 +1,20 @@
+/***Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.***\
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        int n = s.length();
+        unordered_map<char, int> counts;
+        for (int i = 0; i < n; i++) {
+            counts[s[i]]++;
+            counts[t[i]]--;
+        }
+        for (auto count : counts)
+            if (count.second) return false;
+        return true;
+    }
+};
+
+ 
