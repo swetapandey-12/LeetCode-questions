43a4ecc61a0e28c76367c16ca5783f572a25412b
sweta pandey <74544867+swetapandey-12@users.noreply.github.com>
2022-08-22 16:46:23 +0530

    Update 112. Path Sum.cpp

--- 112. Path Sum.cpp
+++ 112. Path Sum.cpp
@@ -1,1 +1,16 @@
 
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root==NULL) 
+            return false;
+        else if(root->val==targetSum && root->left==NULL && root->right==NULL)
+            return true;
+        else
+        {
+           int newTarget = targetSum-root->val;
+             return hasPathSum(root->left, newTarget) || hasPathSum(root->right, newTarget);
+        }
+        
+    }
+};
