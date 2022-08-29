5b22f586beea6ba09aba884a1251ae8258ebb6a2
sweta pandey <74544867+swetapandey-12@users.noreply.github.com>
2022-08-29 18:22:48 +0530

    Create convert array into zig-zag.cpp

---
+++ convert array into zig-zag.cpp
@@ -0,0 +1,19 @@
+class Solution{
+public:	
+	// Program for zig-zag conversion of array
+	void zigZag(int arr[], int n) {
+	    
+	    for(int i = 0; i<n-1; i++){
+	        if(i%2==0 && arr[i]>arr[i+1])
+	        {
+	                swap(arr[i], arr[i+1]);
+	        }
+	        else if(i%2==1 && arr[i]<arr[i+1])
+	        {
+	                swap(arr[i], arr[i+1]);
+	       }
+	       
+	   }
+	   return;
+	}
+};
